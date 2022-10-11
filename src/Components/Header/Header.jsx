2baadf44ee23803963d1f0c953f25fdd6dd0cee7
likhonalmamun <alikhon055@gmail.com>
2022-10-11 08:13:41 +0600

    home page heading section complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,25 +3,25 @@ import { NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   let activeStyle = {
-    color: "orange",
+    color: "rgb(244, 63, 94)",
     fontStyle: "italic",
   };
   return (
     <div className="py-5 px-[65px] bg-gray-600 flex justify-between items-center">
       <h1 className="text-5xl text-teal-400 font-bold ">
-        Coders<span className="text-orange-500">Zone</span>
+        Coders<span className="text-rose-500">Zone</span>
       </h1>
       <div className="text-xl font-semibold  text-teal-300 gap-12 flex">
         <NavLink
           to="/Home"
-          className="hover:text-orange-400 duration-200"
+          className="hover:text-rose-400 duration-200"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
         >
           Home
         </NavLink>
         <NavLink
           to="/Statistics"
-          className="hover:text-orange-400 duration-200"
+          className="hover:text-rose-400 duration-200"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
         >
           Statistics
@@ -29,9 +29,9 @@ const Header = () => {
         <NavLink
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
           to="/Blogs"
-          className="hover:text-orange-400 duration-200"
+          className="hover:text-rose-400 duration-200"
         >
-          Blog Page
+          Blogs
         </NavLink>
       </div>
     </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const topics = useLoaderData().data;
   console.log(topics);
   return (
-    <div>
-      <h1> {topics.length}</h1>
+    <div className="w-2/3 mx-auto  my-[120px]  text-teal-300 bg-[rgba(10,20,15,0.6)] p-7 pb-[50px] border-2 rounded-xl shadow-md text-7xl font-bold ">
+      <h1 className="m-5"> Welcome To Our Site . </h1>
+      <h1 className="m-5"> Make Your Self More Confident </h1>
+      <h1 className="m-5"> About Skills .</h1>
+      <Link
+        to="/github-account"
+        className="text-2xl font-bold py-5 px-10 hover:bg-transparent hover:border-pink-600 hover:border-2 hover:text-rose-400 bg-rose-500 text-teal-100 m-5 rounded-md  "
+      >
+        Visit Our Github
+      </Link>
     </div>
   );
 };
