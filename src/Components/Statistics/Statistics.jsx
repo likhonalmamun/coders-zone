6fed18be416ed4744d25b62e31f31fcf1ed5ef75
likhonalmamun <alikhon055@gmail.com>
2022-10-11 22:51:31 +0600

    made  statistic page responsive

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -15,39 +15,41 @@ const Statistics = () => {
   return (
     <>
       <div
-        className="w-fit mx-auto   mt-[50px]  text-rose-500 bg-[rgba(10,20,15,0.6)] p-5 
-       border-y-2 rounded-xl shadow-md md:text-3xl lg:text-5xl font-bold "
+        className="m-4 mt-[85px] sm:mx-auto sm:w-[80%] md:w-[65%] xl:w-[40%] text-rose-500 bg-[rgba(10,20,15,0.6)] p-5 
+       border-y-2 rounded-xl shadow-md text-2xl sm:text-3xl lg:text-5xl font-bold "
       >
         <h1 className="text-center">Here is the Statistics of our Quiz .</h1>
       </div>
-      <ResponsiveContainer
-        className="mx-auto  text-rose-500 bg-white border-2 rounded-3xl m-5 border-rose-600"
-        width="70%"
-        height={450}
-      >
-        <AreaChart
-          width={800}
-          height={500}
-          data={data}
-          margin={{
-            top: 25,
-            right: 30,
-            left: 0,
-            bottom: 40,
-          }}
+      <div className="m-2 sm:mx-auto sm:w-[80%] md:w-[65%] xl:w-[40%]">
+        <ResponsiveContainer
+          className=" text-rose-500 bg-white border-2 rounded-3xl  border-rose-600"
+          width="100%"
+          height={400}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="total"
-            stroke="rgb(94, 234 ,212)"
-            fill="rgb(225, 29, 72)"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+          <AreaChart
+            width={800}
+            height={500}
+            data={data}
+            margin={{
+              top: 25,
+              right: 30,
+              left: 0,
+              bottom: 40,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="total"
+              stroke="rgb(94, 234 ,212)"
+              fill="rgb(225, 29, 72)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </>
   );
 };
